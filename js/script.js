6ba1c63915df90d86b0f75706925c92be78b1c86
eazaykova <lena.zaykova.95@gmail.com>
2024-01-15 17:58:45 +0700

    Added reproductions section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,238 @@
+const reproductions = [
+  {
+    country: "france",
+    data: [
+      {
+        img: {
+          mobile: "../assets/images/hunting-cupid-mobile.png",
+          tableS: "../assets/images/hunting-cupid-table-510.png",
+          tableM: "../assets/images/hunting-cupid-table-690.png",
+          full: "../assets/images/hunting-cupid-full.png",
+        },
+        author: "Марсель Руссо",
+        name: "Охота Амура",
+        info: "Холст, масло (50х80)",
+        price: 14500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/lady-with-a-dog-mobile.png",
+          tableS: "../assets/images/lady-with-a-dog-table-510.png",
+          tableM: "../assets/images/lady-with-a-dog-table-690.png",
+          full: "../assets/images/lady-with-a-dog-full.png",
+        },
+        author: "Анри Селин",
+        name: "Дама с собачкой",
+        info: "Акрил, бумага (50х80)",
+        price: 16500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/procedure-mobile.png",
+          tableS: "../assets/images/procedure-table-510.png",
+          tableM: "../assets/images/procedure-table-690.png",
+          full: "../assets/images/procedure-full.png",
+        },
+        author: "Франсуа Дюпон",
+        name: "Процедура",
+        info: "Цветная литография (40х60)",
+        price: 20000,
+      },
+      {
+        img: {
+          mobile: "../assets/images/rose-mobile.png",
+          tableS: "../assets/images/rose-table-510.png",
+          tableM: "../assets/images/rose-table-690.png",
+          full: "../assets/images/rose-full.png",
+        },
+        author: "Луи Детуш",
+        name: "Роза",
+        info: "Бумага, акрил (50х80)",
+        price: 12000,
+      },
+      {
+        img: {
+          mobile: "../assets/images/bird-is-meal-mobile.png",
+          tableS: "../assets/images/bird-is-meal-table-510.png",
+          tableM: "../assets/images/bird-is-meal-table-690.png",
+          full: "../assets/images/bird-is-meal-full.png",
+        },
+        author: "Франсуа Дюпон",
+        name: "Птичья трапеза",
+        info: "Цветная литография (40х60)",
+        price: 22500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/landscape-with-fish-mobile.png",
+          tableS: "../assets/images/landscape-with-fish-table-510.png",
+          tableM: "../assets/images/landscape-with-fish-table-690.png",
+          full: "../assets/images/landscape-with-fish-full.png",
+        },
+        author: "Пьер Моранж",
+        name: "Пейзаж с рыбой",
+        info: "Цветная литография (40х60)",
+        price: 20000,
+      },
+    ],
+  },
+  {
+    country: "germany",
+    data: [
+      {
+        img: {
+          mobile: "../assets/images/above-the-city-mobile.png",
+          tableS: "../assets/images/above-the-city-table-510.png",
+          tableM: "../assets/images/above-the-city-table-690.png",
+          full: "../assets/images/above-the-city-full.png",
+        },
+        author: "Курт Вернер",
+        name: "Над городом",
+        info: "Цветная литография (40х60)",
+        price: 16000,
+      },
+      {
+        img: {
+          mobile: "../assets/images/chicks-mobile.png",
+          tableS: "../assets/images/chicks-table-510.png",
+          tableM: "../assets/images/chicks-table-690.png",
+          full: "../assets/images/chicks-full.png",
+        },
+        author: "Макс Рихтер",
+        name: "Птенцы",
+        info: "Холст, масло (50х80)",
+        price: 14500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/among-the-leaves-mobile.png",
+          tableS: "../assets/images/among-the-leaves-table-510.png",
+          tableM: "../assets/images/among-the-leaves-table-690.png",
+          full: "../assets/images/among-the-leaves-full.png",
+        },
+        author: "Мартин Майер",
+        name: "Среди листьев",
+        info: "Цветная литография (40х60)",
+        price: 20000,
+      },
+      {
+        img: {
+          mobile: "../assets/images/bright-bird-mobile.png",
+          tableS: "../assets/images/bright-bird-table-510.png",
+          tableM: "../assets/images/bright-bird-table-690.png",
+          full: "../assets/images/bright-bird-full.png",
+        },
+        author: "Герман Беккер",
+        name: "Яркая птица",
+        info: "Цветная литография (40х60)",
+        price: 13000,
+      },
+      {
+        img: {
+          mobile: "../assets/images/woodpeckers-mobile.png",
+          tableS: "../assets/images/woodpeckers-table-510.png",
+          tableM: "../assets/images/woodpeckers-table-690.png",
+          full: "../assets/images/woodpeckers-full.png",
+        },
+        author: "Вульф Бауэр",
+        name: "Дятлы",
+        info: "Бумага, акрил (50х80)",
+        price: 20000,
+      },
+      {
+        img: {
+          mobile: "../assets/images/big-waters-mobile.png",
+          tableS: "../assets/images/big-waters-table-510.png",
+          tableM: "../assets/images/big-waters-table-690.png",
+          full: "../assets/images/big-waters-full.png",
+        },
+        author: "Вальтер Хартманн",
+        name: "Большие воды",
+        info: "Бумага, акрил (50х80)",
+        price: 23000,
+      },
+    ],
+  },
+  {
+    country: "england",
+    data: [
+      {
+        img: {
+          mobile: "../assets/images/wild-animal-mobile.png",
+          tableS: "../assets/images/wild-animal-table-510.png",
+          tableM: "../assets/images/wild-animal-table-690.png",
+          full: "../assets/images/wild-animal-full.png",
+        },
+        author: "Пол Смит",
+        name: "Дикий зверь",
+        info: "Акварель, бумага (50х80)",
+        price: 19500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/rocky-coast-mobile.png",
+          tableS: "../assets/images/rocky-coast-table-510.png",
+          tableM: "../assets/images/rocky-coast-table-690.png",
+          full: "../assets/images/rocky-coast-full.png",
+        },
+        author: "Джон Уайт",
+        name: "Скалистый берег",
+        info: "Цветная литография (40х60)",
+        price: 17500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/river-and-mountains-mobile.jpg",
+          tableS: "../assets/images/river-and-mountains-table-510.jpg",
+          tableM: "../assets/images/river-and-mountains-table-690.jpg",
+          full: "../assets/images/river-and-mountains-full.jpg",
+        },
+        author: "Джим Уотсон",
+        name: "Река и горы",
+        info: "Акварель, бумага (50х80)",
+        price: 20500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/white-parrot-mobile.png",
+          tableS: "../assets/images/white-parrot-table-510.png",
+          tableM: "../assets/images/white-parrot-table-690.png",
+          full: "../assets/images/white-parrot-full.png",
+        },
+        author: "Юджин Зиллион",
+        name: "Белый попугай",
+        info: "Цветная литография (40х60)",
+        price: 15500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/night-fish-mobile.png",
+          tableS: "../assets/images/night-fish-table-510.png",
+          tableM: "../assets/images/night-fish-table-690.png",
+          full: "../assets/images/night-fish-full.png",
+        },
+        author: "Эрик Гиллман",
+        name: "Ночная рыба",
+        info: "Бумага, акрил (50х80)",
+        price: 12500,
+      },
+      {
+        img: {
+          mobile: "../assets/images/ginger-cat-mobile.png",
+          tableS: "../assets/images/ginger-cat-table-510.png",
+          tableM: "../assets/images/ginger-cat-table-690.png",
+          full: "../assets/images/ginger-cat-full.png",
+        },
+        author: "Альфред Барр",
+        name: "Рыжий кот",
+        info: "Цветная литография (40х60)",
+        price: 21000,
+      },
+    ],
+  },
+  ,
+];
+
 // Меню бургер
 const iconMenu = document.querySelector(".menu__icon");
 const bodyMenu = document.querySelector(".menu__body");
@@ -10,4 +245,104 @@ if (iconMenu) {
     bodyMenu.classList.toggle("_active");
     basketHeader.classList.toggle("_hide");
   });
+}
+
+// Репродукции
+let valueBadge = 0;
+const basket = { france: [], germany: [], england: [] };
+const badgeBasket = basketHeader.querySelector(".basket__badge");
+let currentCountry = "france";
+
+const reproButtons = document.getElementsByClassName(
+  "reproductions__buttons"
+)[0];
+reproButtons.addEventListener("click", handleClick);
+
+const reproList = document.getElementById("repro_list");
+const template = document.querySelector("#template");
+
+if (reproList) {
+  showReproduction(currentCountry);
+}
+
+function getFormattingAmount(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function handleClick() {
+  if (event.target.tagName !== "BUTTON") return;
+  let id = event.target.id;
+  if (currentCountry === id) return;
+
+  for (let element of event.currentTarget.children) {
+    if (element.id === id) {
+      element.classList.add("button_active");
+    } else {
+      element.classList.remove("button_active");
+    }
+  }
+
+  currentCountry = id;
+  showReproduction(currentCountry);
+}
+
+function showReproduction(country) {
+  reproList.innerHTML = "";
+  reproductions
+    .filter((reproduction) => reproduction.country === country)[0]
+    .data.forEach((item) => {
+      const node = template.content.cloneNode(true);
+      const source = node.querySelectorAll("source");
+      const img = node.querySelector("img");
+      const p = node.querySelectorAll("p");
+      const button = node.querySelector("button");
+
+      source[0].srcset = item.img.full;
+      source[1].srcset = item.img.tableM;
+      source[2].srcset = item.img.tableS;
+      source[3].srcset = item.img.mobile;
+
+      img.src = item.img.full;
+      img.alt = `${item.author} - ${item.name}`;
+
+      p[0].textContent = item.author;
+      p[1].textContent = item.name;
+      p[2].textContent = item.info;
+      p[3].textContent = `${getFormattingAmount(item.price)} руб`;
+
+      let isInBasket = basket[currentCountry].includes(item.name);
+      if (isInBasket) {
+        button.classList.add("button_active");
+        button.innerHTML = "В корзине";
+      }
+      button.addEventListener("click", () => toggleToBasket(item.name));
+
+      reproList.append(node);
+    });
+}
+
+function toggleToBasket(name) {
+  const toggle = event.target.classList.toggle("button_active");
+  if (toggle) {
+    event.target.innerHTML = "В корзине";
+    valueBadge += 1;
+    basket[currentCountry].push(name);
+  } else {
+    event.target.innerHTML = "В корзину";
+    valueBadge -= 1;
+    basket[currentCountry] = basket[currentCountry].filter(
+      (item) => item !== name
+    );
+  }
+
+  badgeBasket.innerHTML = valueBadge;
+
+  if (!valueBadge) {
+    badgeBasket.classList.remove("basket__badge_show");
+    return;
+  }
+
+  if (!badgeBasket.classList.contains("basket__badge_show") && valueBadge) {
+    badgeBasket.classList.add("basket__badge_show");
+  }
 }
